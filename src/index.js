9b08e251f857d03a9f995907e58552e7e0ca8414
Terrell Cooper <TerrellCoop@gmail.com>
2021-01-11 13:15:21 -0700

    created folder of pages and containers, moved over jumbotron

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'normalize.css'
 import App from './app';
 import { GlobalStyles } from './global-styles'
 
 
-render(<>
+render(
+    <>
     <GlobalStyles/>
     <App />
     </>, 

07bfa9283f96de5b95769aada67c94691625dfec
Terrell Cooper <TerrellCoop@gmail.com>
2021-03-30 12:25:14 -0600

    Header is styled with minor bug fixes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HeaderContainer } from '../containers/header';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 import JumbotronContainer from '../containers/jumbotron';
@@ -6,6 +7,9 @@ import JumbotronContainer from '../containers/jumbotron';
 export default function Home(){
     return (
         <>
+            <HeaderContainer>
+                <p>Hello</p>
+            </HeaderContainer>
             <JumbotronContainer/>
             <FaqsContainer/>
             <FooterContainer/>
